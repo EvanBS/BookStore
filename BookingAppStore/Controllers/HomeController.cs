300ec9efa79bcf5628de4e34ad95583be5f27501
EvanBS <evandiesp@gmail.com>
2018-10-30 18:52:56 +0200

    Begin

--- BookingAppStore/Controllers/HomeController.cs
+++ BookingAppStore/Controllers/HomeController.cs
@@ -3,28 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using BookingAppStore.Models;
+using System.Threading.Tasks;
 
 namespace BookingAppStore.Controllers
 {
     public class HomeController : Controller
     {
+        BookContext db = new BookContext();
+
         public ActionResult Index()
         {
-            return View();
-        }
+            var books = db.Books;
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Books = books;
 
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpGet]
+        public ActionResult Buy(int id)
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.BookID = id;
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Buy(Purchase purchase)
+        {
+
+            purchase.PurchaseId = db.Purchases.Count() + 1;
+
+            purchase.Date = DateTime.Now;
+
+            db.Purchases.Add(purchase);
+
+            db.SaveChanges();
+
+            return Content($"thanks, {purchase.Name} purch id {purchase.PurchaseId} " +
+                $"book id {purchase.BookId}");
         }
     }
 }
---
+++ BookingAppStore/Models/Purchase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingAppStore.Models
+{
+    public class Purchase
+    {
+        public int PurchaseId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Adderss { get; set; }
+
+        public int BookId { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
